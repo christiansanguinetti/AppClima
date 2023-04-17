2b0a86058eabba710dad61f0d9a99109a3a086c3
christiansanguinetti <christiansanguinetti882@gmail.com>
2023-04-17 17:49:55 -0300

    c

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 
 const {leerInput, inquirerMenu, pausa} = require('./helpers/inquirer');
-
+const Busquedas = require ('./models/Busquedas')
 const main = async() => {
- 
+  const busquedas =  new Busquedas();
+
+
+
   let opt;
 
   do {
    opt = await inquirerMenu();
+  switch (opt){
+    case 1:
+   
+    const lugar = await leerInput('ciudad: ');
+    console.log(lugar)
+
+
+
+
+        console.log('\nInformacion de la ciudad\n'.green);
+        console.log('ciudad')
+        console.log('lat')
+        console.log('lng')
+        console.log('temperatura')
+        console.log('Maxima')
+        console.log('Minima')
+  } 
+
+
+
+
    console.log(opt);
 
    if (opt !==0) await pausa();
---
+++ models/Busquedas.js
@@ -0,0 +1,17 @@
+class Busquedas {
+Historial =  [];
+
+constuctor() {
+    //TODO Leer Db si existe
+
+}
+
+async ciudad (lugar = ''){
+console.log(lugar);
+
+return []
+}
+}
+
+
+module.exports = Busquedas
